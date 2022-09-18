4e267576895be77d2cdcc58e273225844f7f6321
PATEL96 <patel.raj.official.96@gmail.com>
2022-09-18 23:02:02 +0530

    feat: Added New Accordians

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
@@ -15,145 +16,35 @@ const Navbar = () => {
                     className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarNavDropdown"
-                >
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Department 1
-                            </a>
-                            <ul
-                                className="dropdown-menu"
-                                aria-labelledby="navbarDropdownMenuLink"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
+                        <li className="nav-item">
+                            <Link href="/">
+                                <a
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    href="/"
+                                >
+                                    Departments
+                                </a>
+                            </Link>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Department 2
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Features
                             </a>
-                            <ul
-                                className="dropdown-menu"
-                                aria-labelledby="navbarDropdownMenuLink"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Department 3
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Pricing
                             </a>
-                            <ul
-                                className="dropdown-menu"
-                                aria-labelledby="navbarDropdownMenuLink"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdownMenuLink"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Department 4
-                            </a>
-                            <ul
-                                className="dropdown-menu"
-                                aria-labelledby="navbarDropdownMenuLink"
-                            >
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
                     </ul>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Navbar from "../Components/Navbar";
+import Department from "./Depatments/Department";
+import styles from "../styles/Home.module.css";
 
 function IndexPage() {
     return (
-        <>
+        <div className={styles.container}>
             <div className="container-fluid bg-primary">
                 <Navbar />
             </div>
-        </>
+            <Department />
+        </div>
     );
 }
 
